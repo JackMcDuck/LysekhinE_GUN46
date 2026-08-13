11b713be64a99ce348d9a0df6bb598d10123941b
JackMcDuck <jackmduck@gmail.com>
2026-08-14 02:22:39 +0300

    add binarywise operation calculator

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Type first number: ");
+        if (!int.TryParse(Console.ReadLine(), out int a))
+        {
+            Console.WriteLine("Not a number");
+            return;
+        }
+        
+        Console.WriteLine("\nType second number: ");
+        if (!int.TryParse(Console.ReadLine(), out int b))
+        {
+            Console.WriteLine("Not a number");
+            return;
+        }
+
+        Console.WriteLine("\nType bitwise operation symbol(&, |, ^)");
+        string? symbol = Console.ReadLine();
+        
+        switch (symbol)
+        {
+            case "&":
+                Console.WriteLine($"\n--> Your operation in binary: {Convert.ToString(a, 2)} & {Convert.ToString(b, 2)}\n");
+                int result = a & b;
+                Console.WriteLine($"Result:\nDecimal (Base-10): {result}");
+                Console.WriteLine($"Binary (Base-2): {Convert.ToString(result, 2)}");
+                Console.WriteLine($"Hexadecimal (Base-16): {result:X}");
+                break;
+            case "|":
+                Console.WriteLine($"\n--> Your operation in binary: {Convert.ToString(a, 2)} ^ {Convert.ToString(b, 2)}\n");
+                result = a ^ b;
+                Console.WriteLine($"Result:\nDecimal (Base-10): {result}");
+                Console.WriteLine($"Binary (Base-2): {Convert.ToString(result, 2)}");
+                Console.WriteLine($"Hexadecimal (Base-16): {result:X}");
+                break;
+            case "^":
+                Console.WriteLine($"\n--> Your operation in binary: {Convert.ToString(a, 2)} ^ {Convert.ToString(b, 2)}\n");
+                result = a ^ b;
+                Console.WriteLine($"Result:\nDecimal (Base-10): {result}");
+                Console.WriteLine($"Binary (Base-2): {Convert.ToString(result, 2)}");
+                Console.WriteLine($"Hexadecimal (Base-16): {result:X}");
+                break;
+            default:
+                Console.WriteLine("Unknown operation");
+                break;
+        }
+
+    }
+}
